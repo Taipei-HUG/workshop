1a5c6bfc1cf791873d97b0e61e6c7ea573b57b20
Yuren Ju <yurenju@gmail.com>
2019-05-31 22:05:45 +0800

    add revoke command

--- vault/CH02/index.js
+++ vault/CH02/index.js
@@ -9,7 +9,7 @@ const vault = Vault({ token: VAULT_TOKEN });
 
 async function main() {
   const credential = await vault.read("database/creds/my-role");
-  console.log(credential.data);
+  console.log(JSON.stringify(credential, null, 2));
 
   const { username, password } = credential.data;
   const conn = await mysql.createConnection({
@@ -23,7 +23,9 @@ async function main() {
 
   await vault.revoke({ lease_id: credential.lease_id });
   console.log(
-    `use this command to access mysql: mysql -u ${username} -P 3307 -h 127.0.0.1 -p"${password}"`
+    `use this command to access mysql & revoke credential: \n` +
+      `- mysql -u ${username} -P 3307 -h 127.0.0.1 -p"${password}"\n` +
+      `- vault lease revoke ${credential.lease_id}`
   );
   process.exit(0);
 }
