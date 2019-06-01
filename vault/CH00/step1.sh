0990b4e6f3ac2f2a425098dda7fb0dd28dbf66e6
smalltown <smalltown20110306@gmail.com>
2019-06-01 09:48:07 +0800

    add back vault binary in step1.sh

--- vault/CH00/step1.sh
+++ vault/CH00/step1.sh
@@ -2,7 +2,9 @@
 
 mkdir ~/bin
 
-echo "Get binary of kubectl, eksctl, aws-iam-authenticator ..."
+echo "Get binary of vault, kubectl, eksctl, aws-iam-authenticator ..."
+
+wget https://releases.hashicorp.com/vault/1.1.2/vault_1.1.2_linux_amd64.zip && unzip vault_1.1.2_linux_amd64.zip && chmod +x vault && sudo mv vault /usr/local/bin && rm vault_1.1.2_linux_amd64.zip
 
 wget https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl ~/bin/
 
